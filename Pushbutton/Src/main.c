08706032c591253efa6a4d390d89151d2de056f8
VaibhavAttre <68971029+VaibhavAttre@users.noreply.github.com>
2026-04-30 12:22:04 -0700

    updated addresses

--- Pushbutton/Src/main.c
+++ Pushbutton/Src/main.c
@@ -30,7 +30,6 @@
 #define USART3_ISR      ((volatile uint32_t*) 0x4000481C)
 #define USART3_RDR      ((volatile uint32_t*) 0x40004824)
 
-
 /* =========================
    Pin Choices
    ========================= */
@@ -38,9 +37,9 @@
 #define LED_PIN         0   // PC0  (Arduino A1)
 #define BUTTON_PIN      6   // PA6  (Arduino A0)
 
-#define USART3_TX_PIN   8  // PB10
-#define USART3_RX_PIN   9  // PB11
-
+/* USART3 through ST-LINK USB Virtual COM Port */
+#define USART3_TX_PIN   8   // PD8  = USART3_TX
+#define USART3_RX_PIN   9   // PD9  = USART3_RX
 
 /* =========================
    Bit Masks
@@ -48,8 +47,8 @@
 
 /* RCC_AHB2ENR bits */
 #define GPIOA_EN        (1U << 0)
-#define GPIOB_EN        (1U << 1)
 #define GPIOC_EN        (1U << 2)
+#define GPIOD_EN        (1U << 3)
 
 /* RCC_APB1LENR bits */
 #define USART3_EN       (1U << 18)
@@ -66,19 +65,13 @@
 #define LED_SET         (1U << LED_PIN)
 #define LED_RESET       (1U << (LED_PIN + 16))
 
-
 /* =========================
-   Delay
+   System State
    ========================= */
 
-static void delay(volatile uint32_t count)
-{
-    while (count--);
-}
-
 typedef enum {
-	STATE_POSITIVE,
-	STATE_NEGATIVE
+    STATE_POSITIVE = 0,
+    STATE_NEGATIVE = 1
 } SystemState;
 
 /* =========================
@@ -87,61 +80,91 @@ typedef enum {
 
 int main(void)
 {
-    /* --- 1. Enable clocks for GPIOA, GPIOB, GPIOC and USART3 --- */
-    *RCC_AHB2ENR  |= GPIOA_EN | GPIOB_EN | GPIOC_EN;
+    /* --- 1. Enable clocks for GPIOA, GPIOC, GPIOD and USART3 --- */
+    *RCC_AHB2ENR  |= GPIOA_EN | GPIOC_EN | GPIOD_EN;
     *RCC_APB1LENR |= USART3_EN;
 
     /* --- 2. Configure PC0 (LED) as output --- */
-    *GPIOC_MODER &= ~(0x3U << (LED_PIN * 2));   // clear
+    *GPIOC_MODER &= ~(0x3U << (LED_PIN * 2));   // clear PC0 mode bits
     *GPIOC_MODER |=  (0x1U << (LED_PIN * 2));   // 01 = output
 
     /* --- 3. Configure PA6 (button) as input --- */
     *GPIOA_MODER &= ~(0x3U << (BUTTON_PIN * 2)); // 00 = input
 
-    /* --- 4. Pull-up on PA6 (floats HIGH, goes LOW when button pressed) --- */
+    /* --- 4. Pull-up on PA6
+       Not pressed = 1
+       Pressed     = 0
+    */
     *GPIOA_PUPDR &= ~(0x3U << (BUTTON_PIN * 2));
     *GPIOA_PUPDR |=  (0x1U << (BUTTON_PIN * 2)); // 01 = pull-up
 
-    /* --- 5. Configure PD8/PD9 as AF7 (USART3) --- */
-    *GPIOB_MODER &= ~((0x3U << (USART3_TX_PIN * 2)) |
+    /* --- 5. Configure PD8/PD9 as AF7 for USART3 through ST-LINK USB --- */
+
+    /* Set PD8 and PD9 to alternate function mode: 10 */
+    *GPIOD_MODER &= ~((0x3U << (USART3_TX_PIN * 2)) |
                       (0x3U << (USART3_RX_PIN * 2)));
-    *GPIOB_MODER |=  ((0x2U << (USART3_TX_PIN * 2)) |  // 10 = AF
+
+    *GPIOD_MODER |=  ((0x2U << (USART3_TX_PIN * 2)) |
                       (0x2U << (USART3_RX_PIN * 2)));
 
-    *GPIOB_AFRH  &= ~((0xFU << ((USART3_TX_PIN - 8) * 4)) |
-                      (0xFU << ((USART3_RX_PIN - 8) * 4)));
-    *GPIOB_AFRH  |=  ((7U   << ((USART3_TX_PIN - 8) * 4)) |  // AF7
-                      (7U   << ((USART3_RX_PIN - 8) * 4)));
+    /* Select AF7 for PD8 and PD9 */
+    *GPIOD_AFRH &= ~((0xFU << ((USART3_TX_PIN - 8) * 4)) |
+                     (0xFU << ((USART3_RX_PIN - 8) * 4)));
 
-    *GPIOB_PUPDR &= ~(0x3U << (USART3_RX_PIN * 2));
-    *GPIOB_PUPDR |=  (0x1U << (USART3_RX_PIN * 2));    // pull-up on RX
+    *GPIOD_AFRH |=  ((7U << ((USART3_TX_PIN - 8) * 4)) |
+                     (7U << ((USART3_RX_PIN - 8) * 4)));
 
-    /* --- 6. Configure USART3 at 115200 baud (16 MHz HSI) --- */
+    /* Pull-up on RX pin PD9 because UART idle is HIGH */
+    *GPIOD_PUPDR &= ~(0x3U << (USART3_RX_PIN * 2));
+    *GPIOD_PUPDR |=  (0x1U << (USART3_RX_PIN * 2));
+
+    /* --- 6. Configure USART3 at 115200 baud, 8N1, assuming 16 MHz clock --- */
     *USART3_CR1 = 0;
     *USART3_CR2 = 0;
     *USART3_CR3 = 0;
-    *USART3_BRR = 139;   // 16000000 / 115200 ≈ 139
+
+    /*
+       Baud rate divider:
+       16,000,000 / 115,200 ≈ 139
+    */
+    *USART3_BRR = 556;
+
+    /* Enable USART3, receiver, and transmitter */
     *USART3_CR1 = USART_CR1_UE | USART_CR1_RE | USART_CR1_TE;
 
-    /* --- 7. LED off to start --- */
-    *GPIOC_BSRR = LED_RESET;
-
-    /* --- 8. Main loop --- */
-        uint32_t led_state = STATE_POSITIVE;
-        uint32_t current_button = 1;
+    /* --- 7. Initial system state --- */
+    SystemState state = STATE_POSITIVE;   // system initially POSITIVE
+    *GPIOC_BSRR = LED_RESET;              // LED off to start
 
     while (1)
     {
-    	uint32_t pressed = ((*GPIOA_IDR >> BUTTON_PIN) & 1U) == 0;
-    	if (*USART3_ISR & USART_ISR_RXNE){
-    		uint8_t c = (uint8_t)(*USART3_RDR & 0xFF);
-    		if(c == 'p' || c == 'P') {led_state = STATE_POSITIVE;}
-    		else if (c == 'n' || c == 'N') {led_state = STATE_NEGATIVE;}
-    	}
-    	uint32_t led_on;
-    	if (led_state==STATE_POSITIVE){led_on=pressed;}
-    	else {led_on = !pressed;}
+        uint32_t pressed = (((*GPIOA_IDR >> BUTTON_PIN) & 1U) == 0);
 
-    	*GPIOC_BSRR = led_on ? LED_SET : LED_RESET;
+        if ((*USART3_ISR & USART_ISR_RXNE) != 0)
+        {
+            uint8_t c = (uint8_t)(*USART3_RDR & 0xFFU);
+
+            if (c == 'p' || c == 'P')
+            {
+                state = STATE_POSITIVE;
+            }
+            else if (c == 'n' || c == 'N')
+            {
+                state = STATE_NEGATIVE;
+            }
+        }
+
+        uint32_t led_on;
+
+        if (state == STATE_POSITIVE)
+        {
+            led_on = pressed;
+        }
+        else
+        {
+            led_on = !pressed;
+        }
+
+        *GPIOC_BSRR = led_on ? LED_SET : LED_RESET;
     }
 }
