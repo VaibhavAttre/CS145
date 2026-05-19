8cacc3d727b4bf11fc4613ce28bf4ed0ccf3aba4
VaibhavAttre <68971029+VaibhavAttre@users.noreply.github.com>
2026-05-18 19:51:27 -0700

    updated main

--- Calculator/Src/main.c
+++ Calculator/Src/main.c
@@ -1,15 +1,19 @@
 #include "keypad.h"
+#include "lcd.h"
+#include "calculator.h"
 
 int main(void)
 {
+	lcd_init();
     keypad_init();   /* 3 blinks on boot = init OK */
+    calc_init();
 
     while (1)
     {
         char key = keypad_get_key();
 
         if (key != 0)
-            keypad_indicate_key(key);
+            calc_update(key);
 
         keypad_delay_short();
     }
