a530474311582e6e3b8eb19f77b53a52348f3b17
Aaron Perez Jr. Torres <aptorre2@uci.edu>
2026-05-18 19:50:51 -0700

    putu64 finished

--- Calculator/Src/lcd.c
+++ Calculator/Src/lcd.c
@@ -6,13 +6,26 @@
 void lcd_clear_top(void); //clear top row
 void lcd_clear_bottom(void); //clear bottom row
 void lcd_putc(char); //move cursor's right and print input char on cursor
-void lcd_putuint64(uint64_t){
-	//take number 1919
-	//when modding my 10 the lowest digit is taken
-	//copy number digit by digit into buffer array
-	//extend by one using a null terminator to automatically stop insertion
+void lcd_putuint64(uint64_t n){
 
 	char buf[17];
-	int i
+	int i = 16;
+
+	buf[i] = '\0';
+
+	if(n == 0){
+		lcd_putc('0');
+		return;
+	}
+
+	while(n>0){
+		buf[--i] = (char)(('0') + (n % 10));
+		n /= 10;
+	}
+
+	while(buf[i] != '\0'){
+		lcd_putc(buf[i++]);
+	}
+
 }
 void lcd_set_cursor(uint8_t, uint8_t); //(0,0) top left, (1, 0) bottom left
