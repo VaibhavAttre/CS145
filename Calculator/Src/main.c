988ccff38f2a3c9d089d6b1f97358d49b0a8ec49
VaibhavAttre <68971029+VaibhavAttre@users.noreply.github.com>
2026-05-20 18:11:30 -0700

    adkalhdsfkjds

--- Calculator/Src/main.c
+++ Calculator/Src/main.c
@@ -4,18 +4,26 @@
 
 int main(void)
 {
-    lcd_init();
-    keypad_init();
+	lcd_init();
+	keypad_init();
 
     lcd_clear();
 
+    /*
     lcd_set_cursor(0, 0);
     lcd_print("HELLO");
 
     lcd_set_cursor(1, 0);
-    lcd_print("STM32H563ZI");
+    lcd_print("STM32H563ZI");*/
 
     while (1)
     {
+    	char key = keypad_get_key();
+
+        lcd_set_cursor(0, 0);
+        lcd_putc(' ');
+
+        lcd_set_cursor(0, 0);
+        lcd_putc(key);
     }
 }
